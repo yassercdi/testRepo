ae06ab468bcbce59f49b65597e2e408b98354242
yasser <yasser.azaiez@nordnet.fr>
2017-01-09 21:54:03 +0100

    exercice changement dun element dde Dom

--- cour.js
+++ cour.js
@@ -1,3 +1,32 @@
-<script>
-console.log("test git");
-</script>
+function changeColor(e) {
+    //detecter l'element a changer 
+    var divs = document.getElementsByTagName("div");
+
+    var color = "";
+
+    //detecter la touche
+    var caractere = String.fromCharCode(e.charCode)
+
+    switch (caractere) {
+
+
+    case "j":
+        couleur = "yellow";
+        break;
+    case "b":
+        couleur = "blue";
+        break;
+    default:
+        couleur = "red";
+
+
+    }
+    for (var i = 0; i < divs.length; i++) {
+
+        divs[i].style.backgroundColor = couleur;
+
+
+    }
+
+}
+document.addEventListener("keypress", changeColor);
